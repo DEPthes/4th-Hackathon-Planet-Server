b71ffad868314bb357715ca7bdeaa14e5743e3c8
천명현 <cmh1448@naver.com>
2025-06-27 05:08:08 +0900

    feat: Enable JPA auditing in PlanetApplication

--- src/main/java/com/depth/planet/PlanetApplication.java
+++ src/main/java/com/depth/planet/PlanetApplication.java
@@ -2,8 +2,10 @@ package com.depth.planet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PlanetApplication {
 
 	public static void main(String[] args) {
